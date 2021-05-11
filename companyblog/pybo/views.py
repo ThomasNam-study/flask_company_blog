a91e5bf9aa9380b791665287f546f150017a5ae3
red <purred@naver.com>
2021-05-11 11:05:03 +0900

    리스트 작업

--- companyblog/pybo/views.py
+++ companyblog/pybo/views.py
@@ -10,3 +10,10 @@ def index():
     q_list = Question.query.order_by(Question.create_date.desc())
 
     return render_template('question/question_list.html', q_list=q_list)
+
+
+@pybo.route('/detail/<int:question_id>/')
+def detail(question_id):
+    q = Question.query.get_or_404(question_id)
+
+    return render_template('question/question_detail.html', question=q)
