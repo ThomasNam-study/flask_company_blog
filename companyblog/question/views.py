b3ead73f1f3f7b62ceb4b237ab768bd4814f199a
red <purred@naver.com>
2021-05-16 00:37:07 +0900

    정렬 기능 작업 완료

--- companyblog/question/views.py
+++ companyblog/question/views.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
+from sqlalchemy import func
 
 from companyblog import db
-from companyblog.models import Question
+from companyblog.models import Question, Answer, User, question_voter
 from companyblog.question.forms import QuestionForm, AnswerForm
 
 question = Blueprint('question', __name__)
@@ -13,11 +14,43 @@ question = Blueprint('question', __name__)
 @question.route('/')
 def index():
     page = request.args.get('page', type=int, default=1)
+    kw = request.args.get('kw', type=str, default='')
+    so = request.args.get('so', type=str, default='recent')
 
-    q_list = Question.query.order_by(Question.create_date.desc())
+    # q_list = Question.query.order_by(Question.create_date.desc())
+    if so == 'recommand':
+        sub_query = db.session.query(
+            question_voter.c.question.id, func.count('*').label('num_voter')).group_by(
+            question_voter.c.question.id).subquery()
+
+        q_list = Question.query.outerjoin(sub_query, Question.id == sub_query.c.question_id).order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+    elif so == 'popular':
+        sub_query = db.session.query(
+            Answer.question_id, func.count('*').label('num_answer')).group_by(
+            Answer.question_id).subquery()
+
+        q_list = Question.query.outerjoin(sub_query, Question.id == sub_query.c.question_id).order_by(
+            sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:
+        q_list = Question.query.order_by(Question.create_date.desc())
+
+    if kw:
+        search = '%%{}%%'.format(kw)
+
+        sub_query = db.session.query(
+            Answer.question_id, Answer.content, User.username
+        ).join(
+            User, Answer.user_id == User.id
+        ).subquery()
+
+        q_list = q_list.join(User).outerjoin(sub_query, sub_query.c.question_id == Question.id).filter(
+            Question.subject.ilike(search) | Question.content.ilike(search) | User.username.ilike(
+                search) | sub_query.c.content.ilike(search)
+            | sub_query.c.username.ilike(search)).distinct()
+
     q_list = q_list.paginate(page, per_page=10)
 
-    return render_template('question/question_list.html', q_list=q_list)
+    return render_template('question/question_list.html', q_list=q_list, kw=kw, so=so)
 
 
 @question.route('/modify/<int:question_id>/', methods=('GET', 'POST'))
